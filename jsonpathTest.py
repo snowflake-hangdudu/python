0e6777a4398551da2a6d2781b0dbc47de0f66ef5
hangdudu <15551359775@163.com>
2024-01-10 03:18:48 +0800

    111

---
+++ jsonpathTest.py
@@ -0,0 +1,13 @@
+#jsonpath只能解析本地的文件
+import jsonpath
+import json
+
+obj = json.load(open('test.json','r',encoding='utf-8'))
+
+# print(obj)
+
+#拿取person里的name属性
+data = jsonpath.jsonpath(obj,'$..name')
+
+print(data)
+
