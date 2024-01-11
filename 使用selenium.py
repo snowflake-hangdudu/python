0bd5dc368547e1e87f885f8711f78ec79f226f8a
snowflake-hangdudu <15551359775@163.com>
2024-01-11 10:14:35 +0800

    1111

--- 使用selenium.py
+++ 使用selenium.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 
-driver = webdriver.Edge()
+driver = webdriver.Chrome()
 
-driver.get("http://www.baidu.com")
+
+url = "https://www.baidu.com"
+
+driver.get(url)
+
+#元素定位
+button = driver.find_element(By.ID,"kw")
+
+
+print(button.get_attribute('name'))
+
 
 
 
